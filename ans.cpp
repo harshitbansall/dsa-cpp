d1b636b3e05f1d1aa658e40a8fd65743ee4cf494
harshitbansall <harshitbansal587@gmail.com>
2024-11-19 03:26:37 +0530

    Updates

--- ans.cpp
+++ ans.cpp
@@ -2,6 +2,23 @@
 
 using namespace std;
 
+void insertion_sort(int arr[], int n)
+{
+
+  for (int i = 0; i < n; i++)
+  {
+    int j = i;
+    while (j > 0)
+    {
+      if (arr[j - 1] > arr[j])
+      {
+        swap(arr[j - 1], arr[j]);
+      }
+      j--;
+    }
+  }
+}
+
 int main()
 {
   int n;
@@ -13,7 +30,7 @@ int main()
     cin >> arr[i];
   }
 
-  // bubble_sort(arr, n);
+  insertion_sort(arr, n);
 
   for (auto it : arr)
   {
---
+++ util/Sorting/Sorting I/Insertion Sort/Insertion Sort.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void insertion_sort(int arr[], int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        int j = i;
+        while (j > 0)
+        {
+            if (arr[j - 1] > arr[j])
+            {
+                swap(arr[j - 1], arr[j]);
+            }
+            j--;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    insertion_sort(arr, n);
+
+    for (auto it : arr)
+    {
+        cout << it << " ";
+    }
+
+    return 0;
+}
