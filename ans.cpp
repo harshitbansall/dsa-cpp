89f7630974d987a55a843930cea9227faebe210e
harshitbansall <harshitbansal587@gmail.com>
2024-11-19 17:59:07 +0530

    Updates

--- ans.cpp
+++ ans.cpp
@@ -2,35 +2,10 @@
 
 using namespace std;
 
-void insertion_sort(int arr[], int n)
-{
-
-  for (int i = 0; i < n; i++)
-  {
-    int j = i;
-    while (j > 0)
-    {
-      if (arr[j - 1] > arr[j])
-      {
-        swap(arr[j - 1], arr[j]);
-      }
-      j--;
-    }
-  }
-}
-
 int main()
 {
   int n;
   cin >> n;
-  int arr[n];
-
-  for (int i = 0; i < n; i++)
-  {
-    cin >> arr[i];
-  }
-
-  insertion_sort(arr, n);
 
   for (auto it : arr)
   {
